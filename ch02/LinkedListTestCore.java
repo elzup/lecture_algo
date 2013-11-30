1ca0b77672b64839e2a4837407275da2991c0acb
Hiroto Takahashi <guild0105@gmail.com>
2013-11-30 18:11:01 +0900

    note

---
+++ ch02/LinkedListTestCore.java
@@ -0,0 +1,9 @@
+package ch02;
+
+import java.util.LinkedList;
+
+public class LinkedListTestCore {
+    public static void main(String...args) {
+    	LinkedList<Integer> list = new LinkedList<Integer>();
+    }
+}
